3360cfdc46c1809fe521ca7dcbd85d99cba5b705
Nalie <nyapparova@gmail.com>
2021-05-03 12:25:07 +0300

    Update firebase_subscribe.js

--- firebase_subscribe.js
+++ firebase_subscribe.js
@@ -3,6 +3,10 @@ firebase.initializeApp({
     messagingSenderId: '814368183829'
 });
 
+var bt_register = $('#subscribe');
+var bt_delete = $('#delete');
+var token = $('#token');
+
 // браузер поддерживает уведомления
 // вообще, эту проверку должна делать библиотека Firebase, но она этого не делает
 if ('Notification' in window) {
@@ -16,8 +20,27 @@ if ('Notification' in window) {
 
     // по клику, запрашиваем у пользователя разрешение на уведомления
     // и подписываем его
-    $('#subscribe').on('click', function () {
+    bt_register.on('click', function () {
         subscribe();
+    });
+    
+    bt_delete.on('click', function() {
+        // Delete Instance ID token.
+        messaging.getToken()
+            .then(function(currentToken) {
+                messaging.deleteToken(currentToken)
+                    .then(function() {
+                        console.log('Token deleted');
+                        // Once token is deleted update UI.
+                        resetUI();
+                    })
+                    .catch(function(error) {
+                        showError('Unable to delete token', error);
+                    });
+            })
+            .catch(function(error) {
+                showError('Error retrieving Instance ID token', error);
+            });
     });
 }
 
@@ -28,19 +51,31 @@ function subscribe() {
             // получаем ID устройства
             messaging.getToken()
                 .then(function (currentToken) {
-                    console.log(currentToken);
-
-                    if (!currentToken) {
+                    if (currentToken) {
+                        console.log(currentToken);
+                        updateUIForPushEnabled(currentToken);
+                    } else {
                         console.warn('Не удалось получить токен.');
-                        setTokenSentToServer(false);
                     }
                 })
                 .catch(function (err) {
                     console.warn('При получении токена произошла ошибка.', err);
-                    setTokenSentToServer(false);
                 });
     })
     .catch(function (err) {
         console.warn('Не удалось получить разрешение на показ уведомлений.', err);
     });
+}
+
+function updateUIForPushEnabled(currentToken) {
+    console.log(currentToken);
+    token.text(currentToken);
+    bt_register.hide();
+    bt_delete.show();
+}
+
+function resetUI() {
+    token.text('');
+    bt_register.show();
+    bt_delete.hide();
 }
